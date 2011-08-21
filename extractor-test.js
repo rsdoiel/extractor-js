922db8fbad6d29c9555bed99f26f0e199940e993
R. S. Doiel <rsdoiel@gmail.com>
2011-08-21 09:22:22 -0700

    Added two tests for Scrape for selectors in the form of .title > h2 and div.title > h2

--- extractor-test.js
+++ extractor-test.js
@@ -44,19 +44,33 @@ TESTS.FetchPage = function() {
 };
 
 // Test Scrape()
-TESTS.Scrape = function () {
+TESTS.Scrape1 = function () {
 	var doc = [
-	    "<!DOCTYPE html>",
-	    "<html>",
-	    "<head>",
-	    "<title>Test 1</title>",
-	    "</head>",
-	    "<body>",
-	    "<h1>H1 of Test 1</h1>",
-	    "</body>",
-	    "</html>"
+		    "<!DOCTYPE html>",
+		    "<html>",
+		    "<head>",
+		    "<title>Test 1</title>",
+		    "</head>",
+		    "<body>",
+		    "<h1>H1 of Test 1</h1>",
+		    "</body>",
+		    "</html>"
 	    ].join("\n"),
-	    map = {'title':'title','h1':'h1'};
+	    map = { title: 'title', h1: 'h1' },
+	    doc2 = [
+		    "<!DOCTYPE html>",
+		    "<html>",
+		    "<head>",
+		    "<title>Test 1</title>",
+		    "</head>",
+		    "<body>",
+		    "<div class='title'><h2>H2 title</h2> This is more title</div>",
+		    "<div class='article'>This is where an article would go.</div>",
+		    "</body>",
+		    "</html>"
+	    ].join("\n"),
+	    map2a = { title: '.title > h2', article: '.article' },
+	    map2b = { title: 'div.title > h2', article: '.article'};
 	
 	extractor.Scrape("./test-data/test-1.html", map, function (err, data, pname) {
 	    assert.ok(! err, "Should  not get an error. " + err);
@@ -77,7 +91,27 @@ TESTS.Scrape = function () {
 	    test_completed += 1;
 	    display("Scrape test, completed processing (" + test_completed + "/" + test_expected + ") : markup");
 	});
-	return 2;// Two tests in this batch
+	
+	extractor.Scrape(doc2, map2a, function (err, data, pname) {
+	    assert.ok(! err, "Should  not get an error. " + err);
+	    assert.equal(pname, undefined, "Should have pname set to ''");
+	    assert.ok(typeof data === 'object', "Should have a data object");
+	    assert.equal(data.title, "h2 Title", ".title should be 'h2 Title': " + JSON.stringify(data));
+	    assert.equal(data.article, "This is where an article would go.", ".article should be 'This is where an article would go.': " + JSON.stringify(data));
+	    test_completed += 1;
+	    display("Scrape test, completed processing (" + test_completed + "/" + test_expected + ") : markup");
+	});
+
+	extractor.Scrape(doc2, map2a, function (err, data, pname) {
+	    assert.ok(! err, "Should  not get an error. " + err);
+	    assert.equal(pname, undefined, "Should have pname set to ''");
+	    assert.ok(typeof data === 'object', "Should have a data object");
+	    assert.equal(data.title, "h2 Title", "div.title should be 'h2 Title': " + JSON.stringify(data));
+	    assert.equal(data.article, "This is where an article would go.", ".article should be 'This is where an article would go.': " + JSON.stringify(data));
+	    test_completed += 1;
+	    display("Scrape test, completed processing (" + test_completed + "/" + test_expected + ") : markup");
+	});
+	return 4;// Two tests in this batch
 };
 
 
