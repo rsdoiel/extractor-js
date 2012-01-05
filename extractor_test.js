dd01e4e565e07d8927869b040efd87358851da5a
R. S. Doiel <rsdoiel@usc.edu>
2012-01-05 09:43:04 -0800

    Passing tests again as 0.0.7d.

--- extractor_test.js
+++ extractor_test.js
@@ -145,7 +145,7 @@ TESTS.Scrape = function () {
 
 // Tests of Spider()
 TESTS.Spider = function () {
-	test_expected += 2;
+	test_expected += 3;
 	extractor.Spider("http://nodejs.org", function (err, data, pname) {
 		assert.ok(data.anchors, "Should have anchors in page.");
 		assert.ok(data.images, "Should have at least the logo in the page.");
@@ -155,7 +155,17 @@ TESTS.Spider = function () {
 	});
 
 	extractor.Spider("test-data/test-3a.html", function (err, data, pname) {
-		var expected_result = [ 'http://www.usc.edu/its/webservices/','http://nodejs.org','http://go-lang.org','http://www.google.com/chromeos','http://its.usc.edu/~rsdoiel/wscore','wscore/README.txt','http://search.npmjs.org/#/_author/R.%20S.%20Doiel','https://github.com/rsdoiel/extractor-js','https://github.com/rsdoiel/tbone','http://www.npr.org/blogs/inside/2011/02/02/126312263/behind-the-code-avoiding-spaghetti-html','https://github.com/rsdoiel/tbone','https://github.com/rsdoiel/stn','https://github.com/rsdoiel/opt','demo','https://github.com/rsdoiel','cv.html' ], i, pos, anchor;
+		var expected_result = [ 'http://www.usc.edu/its/webservices/', 
+			'http://nodejs.org', 'http://go-lang.org', 
+			'http://www.google.com/chromeos', 
+			'http://its.usc.edu/~rsdoiel/wscore', 'wscore/README.txt', 
+			'http://search.npmjs.org/#/_author/R.%20S.%20Doiel', 
+			'https://github.com/rsdoiel/extractor-js', 
+			'https://github.com/rsdoiel/tbone', 
+			'http://www.npr.org/blogs/inside/2011/02/02/126312263/behind-the-code-avoiding-spaghetti-html', 
+			'https://github.com/rsdoiel/tbone', 'https://github.com/rsdoiel/stn', 
+			'https://github.com/rsdoiel/opt', 'demo', 
+			'https://github.com/rsdoiel', 'cv.html' ], i, pos, anchor;
 		
 		assert.ok(! err, pname + ": " + err);
 		assert.ok(data.anchors, "Should have anchors in page.");
@@ -174,7 +184,59 @@ TESTS.Spider = function () {
 		assert.equal(expected_result.length, 0, "elements left over: " + JSON.stringify(expected_result));		
 		test_completed += 1;
 		display("Spider test-data/test-3a.html completed processing (" + test_completed + "/" + test_expected + ")");
-	});	
+	});
+
+	extractor.Spider("test-data/test-3b.html", function (err, data, pname) {
+		var expected_result = [ 
+			"http://www.usc.edu/web", 
+			"http://tel.usc.edu", 
+			"http://its.usc.edu", 
+			"http://www.usc.edu", 
+			"http://web-app.usc.edu/ws/eo3", 
+			"http://web-app.usc.edu/ws/uscmap", 
+			"wscore", 
+			"http://web-app.usc.edu/web/uscreader/", 
+			"http://web-app.usc.edu/ws/webcams", 
+			"http://web-app.usc.edu/ws/webcams", 
+			"http://mobile.usc.edu", 
+			"http://webster.usc.edu/cms", 
+			"http://its.usc.edu/~rsdoiel/wscore", 
+			"http://web-app.usc.edu/ws/soc", 
+			"http://web-app.usc.edu/ws/eo2/help", 
+			"http://www.usc.edu/hsc/dental/opath/", 
+			"http://its.usc.edu/~rsdoiel/demo", 
+			"http://its.usc.edu/~rsdoiel/cs577/fall2006", 
+			"http://its.usc.edu/~rsdoiel/cs577/fall2005", 
+			"http://its.usc.edu/~rsdoiel/cs577/fall2004", 
+			"http://its.usc.edu/~rsdoiel/cs577/fall2003", 
+			"http://www.cla-net.org/conf/conf.html",
+			"http://its.usc.edu/~rsdoiel/cla2000", 
+			"http://www.educause.edu",
+			"http://www.usc.edu/its/partners/orgs/lacasis", 
+			"http://www.e-learnit.fi/handbok/", 
+			"http://www.builder.com/Graphics/SmilTutorial/?st.bl.fd.sg1.feat.1670", 
+			"http://builder.com/Servers/Internet2/?st.bl.fd.ts1.feat.1678", 
+			"index.html" ], i, pos, anchor;
+		
+		assert.ok(! err, pname + ": " + err);
+		assert.ok(data.anchors, "Should have anchors in page.");
+		assert.ok(! data.images, "Should NOT have images.");
+		assert.ok(data.links, "Should have some links to CSS at least.");
+		assert.equal(expected_result.length, data.anchors.length, "Should have same lengths: " + expected_result.length + " != " + data.anchors.length);
+
+		data.anchors.forEach(function (anchor) {
+			assert.ok(anchor.href, "Should have anchor.href for test-3b.html");
+			pos = expected_result.indexOf(anchor.href);
+			assert.ok(pos >= 0, "Should find " + anchor.href + " in expected array.");
+			if (pos >= 0) {
+				assert.ok(expected_result.splice(pos,1), "Should be able to remove " + pos + " position in expected array.");
+			}
+		});
+		assert.equal(expected_result.length, 0, "elements left over: " + JSON.stringify(expected_result));		
+		test_completed += 1;
+		display("Spider test-data/test-3b.html completed processing (" + test_completed + "/" + test_expected + ")");
+	});
+
 };
 
 // Tests of SubmitForm()
